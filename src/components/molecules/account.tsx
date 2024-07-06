f47406532016005b57ffb61c0366629eb747b22b
prit1310 <pritsenjaliya1162@gmail.com>
2024-07-06 13:50:27 +0530

    get data from firebase realtime database and display

--- src/components/molecules/account.tsx
+++ src/components/molecules/account.tsx
@@ -4,12 +4,28 @@ import {Input} from "@nextui-org/input"
 import { useForm } from "react-hook-form"
 import { ref, set ,get,child} from "firebase/database";
 import { db ,auth} from "../../lib/firebase"
+import { useEffect, useState } from "react";
 
 const Account = () => {
-
+  const [userData,setUserData] = useState({bio:"",username:""})
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const { register, handleSubmit } = useForm();
 
+  useEffect(()=>{
+    get(child(ref(db), `users/${auth.currentUser?.uid}`)).then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log(snapshot.val());
+      setUserData({
+        username:snapshot.val().username,
+        bio:snapshot.val().bio
+      })
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
+  },[])
   async function onSubmit(values:any) {
     set(ref(db, 'users/' + auth.currentUser?.uid), {
       username: values.name,
@@ -17,20 +33,11 @@ const Account = () => {
     });
     console.log(values)
   }
-  get(child(ref(db), `users/${auth.currentUser?.uid}`)).then((snapshot) => {
-    if (snapshot.exists()) {
-      console.log(snapshot.val());
-    } else {
-      console.log("No data available");
-    }
-  }).catch((error) => {
-    console.error(error);
-  });
   
   return (
     <>
-      <h1>Name:</h1>
-      <h1>Bio:</h1>
+      <h1>Name: {userData.username}</h1>
+      <h1>Bio: {userData.bio}</h1>
       <p>Profile image:</p>
 
       <Button onPress={onOpen} color="primary">Update profile</Button>
